ef41676170f75f3585e68ff749e0f58b004f4154
ljaeg <loganjaeger@gmail.com>
2019-07-17 11:36:52 -0700

    attrs

--- LargeFOV/make_norm.py
+++ LargeFOV/make_norm.py
@@ -22,7 +22,7 @@ ValYes = not_normed['ValYes']
 normed = h5.File(os.path.join(Dir, 'FOV100_Num10000_b_normed.hdf5'), 'w')
 normed['FOVSize'] = FOVSize
 normed['NumFOVS'] = NumFOVs
-normed['Foils'] = not_normed['Foils']
+normed['Foils'] = not_normed.attrs['Foils']
 
 def norm(dataset):
 	print(np.mean(dataset))
