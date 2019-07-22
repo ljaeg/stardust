508d9375f02b34374901ae2262e1965ee02326b1
ljaeg <loganjaeger@gmail.com>
2019-07-22 11:09:06 -0700

    norming 40s

--- LargeFOV/make_norm.py
+++ LargeFOV/make_norm.py
@@ -4,7 +4,7 @@ import os
 
 ##We're going to norm the images by per image mean subtraction, and save them to a DIFFERENT directory so that we compare performance
 Dir = '/home/admin/Desktop/Preprocess'
-not_normed = h5.File(os.path.join(Dir, 'FOV100_Num10000_b.hdf5'), 'r')
+not_normed = h5.File(os.path.join(Dir, 'FOV40_Num10000_b.hdf5'), 'r')
 # testdir = '/users/loganjaeger/Desktop/SAH/Code/Current'
 # not_normed = h5.File(os.path.join(testdir, 'Data_1000_craters.hdf5'), 'r')
 
@@ -19,7 +19,7 @@ TestYes = not_normed['TestYes']
 ValNo = not_normed['ValNo']
 ValYes = not_normed['ValYes']
 
-normed = h5.File(os.path.join(Dir, 'FOV100_Num10000_b_normed.hdf5'), 'w')
+normed = h5.File(os.path.join(Dir, 'FOV40_Num10000_b_normed.hdf5'), 'w')
 normed.attrs.create('FOVSize', FOVSize)
 normed.attrs.create("NumFOVs", NumFOVs)
 normed.attrs.create('Foils', Foils)
