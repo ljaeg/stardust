0f5fa7cd5f76b65f057bdb4b671515adf05d227d
ljaeg <loganjaeger@gmail.com>
2019-07-29 16:22:05 -0700

    j

--- LargeFOV/CNN_200.py
+++ LargeFOV/CNN_200.py
@@ -101,8 +101,8 @@ except:
   Foils = DataFile.attrs['Foils']
 # Read the Train/Test/Val datasets.
 num_ims = 850
-TrainNo = DataFile['TrainNo'][:num_ims - 800]
-TrainYes = DataFile['TrainYes'][:num_ims + 800]
+TrainNo = DataFile['TrainNo'][:num_ims - 500]
+TrainYes = DataFile['TrainYes'][:num_ims + 500]
 TestNo = DataFile['TestNo'][:int(num_ims/2)]
 TestYes = DataFile['TestYes'][:int(num_ims/2)]
 ValNo = DataFile['ValNo'][:int(num_ims/2)]
