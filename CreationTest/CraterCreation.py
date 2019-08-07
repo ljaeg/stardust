6c5f062f42c5f20bb7debc396d3224d57094826b
ljaeg <loganjaeger@gmail.com>
2019-08-07 10:13:00 -0700

    d

--- CreationTest/CraterCreation.py
+++ CreationTest/CraterCreation.py
@@ -20,7 +20,7 @@ RunDir = '/home/admin/Desktop/Aug6'
 # shutil.copy(os.path.join(RunDir, 'Data_10000.hdf5'), os.path.join(RunDir, 'Data.hdf5'))
 
 ### LOAD THE HDF.
-DataFile = h5py.File(os.path.join(RunDir, 'Middle_FOV150_Num10k_blanks.hdf5'), 'r+')
+DataFile = h5py.File(os.path.join(RunDir, 'Middle_FOV150_Num10k_new.hdf5'), 'r+')
 #TrainTestValSplit = DataFile.attrs['TrainTestValSplit']
 FOVSize = DataFile.attrs['FOVSize']
 NumFOVs = DataFile.attrs['NumFOVs']
---
+++ CreationTest/pipeline.py
@@ -0,0 +1,6 @@
+import BlankGather.py 
+import CraterCreation.py
+import make_norm.py
+print('!!!!!!!!!!!!!!!!!!!')
+print('all done')
+print('!!!!!!!!!!!!!!!!!!!')
