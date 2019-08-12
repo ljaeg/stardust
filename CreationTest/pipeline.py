c86d22142dd86036067c8f1647ee571779c672f2
ljaeg <loganjaeger@gmail.com>
2019-08-12 11:00:47 -0700

    d

--- CreationTest/pipeline.py
+++ CreationTest/pipeline.py
@@ -2,10 +2,14 @@ import BlankGather
 import CraterCreation
 import make_norm
 FOVSize = 500
-BlankGather.blanks_do(FOVSize)
+#BlankGather.blanks_do(FOVSize)
+print('blanks created')
 make_norm.norm_do(FOVSize)
+print('norm number 1')
 CraterCreation.crater_do(FOVSize)
+print('craters inserted')
 make_norm.norm_do(FOVSize)
+print('normed again')
 print('!!!!!!!!!!!!!!!!!!!')
 print('all done')
 print('!!!!!!!!!!!!!!!!!!!')
