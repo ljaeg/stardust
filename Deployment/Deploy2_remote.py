c524c8ecd8be400056007698be3977c7849b96db
Logan Jaeger <loganjaeger@berkeley.edu>
2020-01-22 12:52:24 -0800

    k

--- Deployment/Deploy2_remote.py
+++ Deployment/Deploy2_remote.py
@@ -29,8 +29,8 @@ config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 
 #Paths
-#chrome_path = "/home/admin/Downloads/chromedriver"
-chrome_path = "/home/admin/Desktop/forChromeDriver/chromedriver"
+chrome_path = "/home/admin/Downloads/chromedriver"
+#chrome_path = "/home/admin/Desktop/forChromeDriver/chromedriver"
 img_path = "/home/admin/Desktop/GH/Deployment/Answers"
 
 
