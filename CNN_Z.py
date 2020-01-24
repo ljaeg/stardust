4fe951da0c8d1e439282abc96c3a183ff238b0d7
Logan Jaeger <loganjaeger@berkeley.edu>
2020-01-23 16:11:29 -0800

    dd

--- CNN_Z.py
+++ CNN_Z.py
@@ -201,7 +201,7 @@ TBLog = TensorBoard(log_dir = '/home/admin/Desktop/TB/July15/tryingonremote/{}'.
 
 model.fit_generator(generator=train_generator,
                    steps_per_epoch=train_generator.n//batch_size,
-                   epochs=250,
+                   epochs=170,
                    verbose=2,
                    validation_data=validation_generator,
                    validation_steps=validation_generator.n//batch_size,
