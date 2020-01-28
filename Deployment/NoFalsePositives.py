16f6a9ee19ce98ab9a2fecd5a556cdf8b42c8cf1
Logan Jaeger <loganjaeger@berkeley.edu>
2020-01-28 11:15:07 -0800

    dd

--- Deployment/NoFalsePositives.py
+++ Deployment/NoFalsePositives.py
@@ -22,9 +22,25 @@ from keras.optimizers import Nadam
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.utils import shuffle
 
+#f1 score
+def f1_acc(y_true, y_pred):
+    # Count positive samples.
+    c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    c2 = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    c3 = K.sum(K.round(K.clip(y_true, 0, 1)))
+    # If there are no true samples, fix the F1 score at 0.
+    if c3 == 0 or c2 == 0:
+        return 0
+    # How many selected items are relevant?
+    precision = c1 / c2
+    # How many relevant items are selected?
+    recall = c1 / c3
+    # Calculate f1_score
+    f1_score = 2 * (precision * recall) / (precision + recall)
+    return f1_score
 
 #load the models for the different sizes
-model150 = load_model('/home/admin/Desktop/Saved_CNNs/NFP_acc_FOV150.h5')
+model150 = load_model('/home/admin/Desktop/Saved_CNNs/NFP_acc_FOV150.h5', custom_objects={'f1_acc': f1_acc})
 model100 = load_model('/home/admin/Desktop/Saved_CNNs/NFP_loss_FOV100.h5', custom_objects={'f1_acc': f1_acc})
 model30 = load_model("/home/admin/Desktop/GH/NFP30_F1.h5", custom_objects={'f1_acc': f1_acc})
 
